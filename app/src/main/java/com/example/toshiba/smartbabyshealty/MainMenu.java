9ea4fe5e32dc558ba88a9b111375bba35eb90bcd
runbow <dragoon.redeyed@gmail.com>
2015-08-15 15:57:28 +0700

    remove appcompat

--- app/src/main/java/com/example/toshiba/smartbabyshealty/MainMenu.java
+++ app/src/main/java/com/example/toshiba/smartbabyshealty/MainMenu.java
@@ -1,11 +1,12 @@
 package com.example.toshiba.smartbabyshealty;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainMenu extends AppCompatActivity {
+public class MainMenu extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
